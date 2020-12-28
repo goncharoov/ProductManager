a4ef3b79155432ad5a7b13314b1c123504eb1a1e
goncharoov <takeyo69@icloud.com>
2020-12-28 19:56:48 +1000

    modified ProductManager and ProductRepository

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,15 +1,36 @@
 package ru.netology.repository;
 
+import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 
 public class ProductRepository {
+
     private Product[] items = new Product[0];
 
-    public void save () {}
-
-    public Product[] findAll(){
-        return items;
+    public void save(Product item) {
+        int length = items.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
 
-    public void removeById(int id) {}
+    public void removeById(int id) {
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
 }
